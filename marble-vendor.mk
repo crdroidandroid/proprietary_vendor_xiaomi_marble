d001961da3c2f4b74b0d909a076a3881be629748
saikiran2001 <bjsaikiran@gmail.com>
2023-12-01 11:27:27 +0000

    Revert "marble: Nuke workload classifier"
    This reverts commit c2d940de8ee50061bb4b74e35431cb0ce1c3822c.
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- marble-vendor.mk
+++ marble-vendor.mk
@@ -88,6 +88,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/marble/proprietary/system_ext/lib64/libqti-iopd-client_system.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libqti-iopd-client_system.so \
     vendor/xiaomi/marble/proprietary/system_ext/lib64/libqti-perfd-client_system.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libqti-perfd-client_system.so \
     vendor/xiaomi/marble/proprietary/system_ext/lib64/libqti_performance.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libqti_performance.so \
+    vendor/xiaomi/marble/proprietary/system_ext/lib64/libqti_workloadclassifiermodel.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libqti_workloadclassifiermodel.so \
     vendor/xiaomi/marble/proprietary/system_ext/lib64/libsd_sdk_display.qti.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libsd_sdk_display.qti.so \
     vendor/xiaomi/marble/proprietary/system_ext/lib64/libsdm-disp-apis.qti.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libsdm-disp-apis.qti.so \
     vendor/xiaomi/marble/proprietary/system_ext/lib64/libskewknob_system.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libskewknob_system.so \
@@ -2732,6 +2733,7 @@ PRODUCT_PACKAGES += \
     QtiTelephonyService \
     colorservice \
     Ims \
+    workloadclassifier \
     dpmserviceapp \
     qcrilmsgtunnel \
     QPerformance \
