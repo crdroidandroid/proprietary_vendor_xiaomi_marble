86a70b86cc7ebc4b425915e2da32466beaf73c95
chaitanyakm <chaitanyaarora3@gmail.com>
2026-04-12 22:33:23 +0200

    marble: initial import from OS3.0.3.0.VMRMIXM

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),marble)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,8856756e5f5361d6dac5014ce275c2c00d1fd11e)
+$(call add-radio-file-sha1-checked,radio/aop.img,d451df92e035f32686b4cbf8d1367e82b400c27b)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,74aa0e5a79c96e4230c6ce333b4425ae8f1506f4)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,46d5183bb120eb1ea1920605459418f55bba4a4f)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,5027e5df4084f64f81c85e05e3c7dc6cb6de6de6)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,64709d87de846c97534981ed64105ed1cd37279f)
+$(call add-radio-file-sha1-checked,radio/dsp.img,abb0cf432db0d513601b5314b55b1765dc900317)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,72df65d149ffeb96fed254bcddbe0f04dbfc9b32)
+$(call add-radio-file-sha1-checked,radio/hyp.img,85c06269cc85e60c474c94f03c22253ea464c398)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,2d1e98470aa4c24e73cb7cc139dfe5a58d904435)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,4fa4c91f4c72d1615e9f5d8521a163291325e207)
+$(call add-radio-file-sha1-checked,radio/modem.img,98c483a9113bac2afd67fe9b32972f53cdcada45)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,e5bc9425096f8ac38e5d85fadefd72f08c121ec4)
+$(call add-radio-file-sha1-checked,radio/shrm.img,94808136cfbc957990d82e854bc09f1e708907bf)
+$(call add-radio-file-sha1-checked,radio/tz.img,00b02366743b607aa0c3619b58f33979d0d9b03b)
+$(call add-radio-file-sha1-checked,radio/uefi.img,b4a6c17d0296583854ed42b4cc29816bd3a856e0)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,94452b512abcc4b0c218b36db439919e3761b7ca)
+$(call add-radio-file-sha1-checked,radio/xbl.img,36fb2c624dcc5c3c91cc4af13241378f01383c2e)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,45687298814f95dc68bef0033f501696b6f1928d)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,ee7d0e60fb995b5c9473be6d9675ca55af5b6a94)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,25 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    shrm \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
